fee57273caa8a5d8d5afe5377dcef8da0a61534f
Dmitry <dmitry27zh@gmail.com>
2023-12-08 12:36:00 +0500

    Add App template

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import './App.css'
 
 function App() {
-  return 'Hi'
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
 }
 
 export default App
